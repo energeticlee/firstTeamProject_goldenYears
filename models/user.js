4c34300c50b95143c6e80873c8d28c7cc8089e9b
Kevin Ho <hokevin78@gmail.com>
2021-07-20 14:12:09 +0800

    Refactored user model
    Added unique option for password field, changed health conditions to String type instead of [String], changed ref of test_1/2/3 from UserSchema to test_#_Schema

--- models/user.js
+++ models/user.js
@@ -4,18 +4,20 @@ const userSchema = new mongoose.Schema({
   id: Number,
   name: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
-  photo: String,
+  password: { type: String, required: true, unique: true },
+  photo: String, //URL
   age: Number,
-  gender: { type: String, required: true },
+  gender: { type: String, required: true }, // Standardise as M/F
   height: { type: Number, required: true },
   weight: { type: Number, required: true },
-  health_conditions: [String],
+  health_conditions: String,
   admin: { type: Boolean, required: true },
   doctor: { type: String, required: true },
-  test_1: [{ type: Schema.Types.ObjectId, ref: "userSchema" }],
-  test_2: [{ type: Schema.Types.ObjectId, ref: "userSchema" }],
-  test_3: [{ type: Schema.Types.ObjectId, ref: "userSchema" }],
+  test_1: [{ type: Schema.Types.ObjectId, ref: "test_1_Schema" }],
+  test_2: [{ type: Schema.Types.ObjectId, ref: "test_2_Schema" }],
+  test_3: [{ type: Schema.Types.ObjectId, ref: "test_3_Schema" }],
 });
 
 const User = mongoose.model("User", userSchema);
+
+module.exports = User;
