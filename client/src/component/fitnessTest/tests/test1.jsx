b628c6795c2e504985885b3df42c90edb22a1073
root <root@DESKTOP-776Q9SU.localdomain>
2021-07-21 18:09:11 +0800

    add dependencies for fitness test

---
+++ client/src/component/fitnessTest/tests/test1.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const test1 = () => {
+    return (
+        <div>
+            <div>30-Second Chair Stand</div>
+            <div>Instructions</div>
+            <div>
+                {/* Camera | PoseEstimation logic */}
+            </div>
+            <div>
+                <div>Time Remaining</div>
+                <div>Score</div>
+                <div>Submit</div>
+            </div>
+        </div>
+    )
+}
+
+export default test1
