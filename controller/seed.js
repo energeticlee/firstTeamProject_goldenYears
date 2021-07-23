411f97f257543d705b34b6e1576f83c46450b905
root <root@DESKTOP-776Q9SU.localdomain>
2021-07-23 17:06:38 +0800

    update userSchema

--- controller/seed.js
+++ controller/seed.js
@@ -3,14 +3,17 @@ const router = express.Router();
 const seedSchema = require("../models/user");
 
 const names = [
-  { name: "Kevin" },
-  { name: "bob" },
-  { name: "farhan" },
-  { name: "ashley" },
-  { name: "justin" },
+  { name: "Kevin", email: "helloKevin@123.com", password: "123" },
+  { name: "bob", email: "helloBob@123.com", password: "1234" },
+  { name: "farhan", email: "helloFarhan@123.com", password: "12356" },
+  { name: "ashley", email: "helloAshley@123.com", password: "123455" },
+  { name: "justin", email: "helloJustin@123.com", password: "124443" },
 ];
 
 router.get("/", (req, res) => {
+  seedSchema.deleteMany({}, (error, data) => {
+    if (error) res.status(400).json({ error: "deleteError" });
+  });
   seedSchema.create(names, (err, nameList) => {
     if (err) {
       console.log(err);
--- models/user.js
+++ models/user.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 // const Schema = mongoose.Schema;
 
-const seedSchema = mongoose.Schema({
-  name: { type: String },
+const userSchema = mongoose.Schema({
+  name: { type: String, require: true },
+  email: { type: String, require: true },
+  password: { type: String, require: true },
+  photo: String,
+  age: Date,
+  gender: String,
+  height: Number,
+  weight: Number,
+  healthCondition: [String],
+  myDoctor: { type: mongoose.Schema.Types.ObjectId, ref: "doctor_Schema" },
 });
 
-module.exports = mongoose.model("seedSchema", seedSchema);
+module.exports = mongoose.model("userSchema", userSchema);
