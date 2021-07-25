19c89ffb71e4a6f81f10aa7308726c0c4c5713c9
“ash1ey” <xy.wang2208@gmail.com>
2021-07-25 22:13:19 +0800

    made some changes to PatientsIntro with Kevin's help

--- client/src/component/mainapp/Landing.js
+++ client/src/component/mainapp/Landing.js
@@ -1,21 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import PatientIntro from "../userIntro/PatientIntro";
-
-const Landing = () => {
-  return (
-    <div>
-      <h1>GoldenYears</h1>
-      <p>
-        Cras ultricies mi eu turpis hendrerit fringilla. Duis leo. Quisque ut
-        nisi. Donec id justo. Nulla neque dolor, sagittis eget, iaculis quis,
-        molestie non, velit.
-      </p>
-
-      <PatientIntro />
-    </div>
-  );
-=======
 import HomeNavBar from "./HomeNavBar";
 
 const Landing = () => {
@@ -30,6 +13,5 @@ const Landing = () => {
 			</p>
 		</>
 	);
->>>>>>> ashley
 };
 export default Landing;
--- client/src/component/userIntro/PatientIntro.js
+++ client/src/component/userIntro/PatientIntro.js
@@ -1,80 +1,47 @@
 import React from "react";
-<<<<<<< HEAD
-import Login from "./PatientLogin";
-import Registration from "./PatientReg";
-import { Link, useRouteMatch } from "react-router-dom";
-import { Switch, Route } from "react-router-dom";
-// import { useState } from "react";
-
-const UserIntro = () => {
-  let { path, url } = useRouteMatch();
-
-  return (
-    <div>
-      <h1>Getting Started</h1>
-      <span>
-        <Link to={`${url}/login`}>Login</Link>
-        <Link to={`${url}/registration`}>Register</Link>
-      </span>
-      <br />
-      <br />
-      <div>
-        <Switch>
-          <Route path={`${path}/login`}>
-            <Login />
-          </Route>
-          <Route path={`${path}/registration`}>
-            <Registration />
-          </Route>
-        </Switch>
-      </div>
-    </div>
-  );
-=======
-import { Link } from "react-router-dom";
+import { Link, Switch, Route, useRouteMatch } from "react-router-dom";
 import PatientLogin from "./PatientLogin";
 import PatientReg from "./PatientReg";
 // import { useState } from "react";
 
 const PatientIntro = () => {
-	// let { path, url } = useRouteMatch();
+	let { path, url } = useRouteMatch();
 
 	return (
-		<>
-			<h1>Getting Started</h1>
-			<span>
-				<Link to="/patients/login" component={PatientLogin}>
-					<button>Login</button>
-				</Link>
-				<Link to="/patients/register" component={PatientReg}>
-					<button>Register</button>
-				</Link>
-			</span>
-		</>
-
 		// <>
 		// 	<h1>Getting Started</h1>
 		// 	<span>
-		// 		<Link to={`${url}/login`}>
+		// 		<Link to="/patients/login" component={PatientLogin}>
 		// 			<button>Login</button>
 		// 		</Link>
-		// 		<Link to={`${url}/registration`}>
+		// 		<Link to="/patients/register" component={PatientReg}>
 		// 			<button>Register</button>
 		// 		</Link>
 		// 	</span>
-		// 	<br />
-		// 	<br />
-		// 	<Switch>
-		// 		<Route path={`${path}/login`}>
-		// 			<Login />
-		// 		</Route>
-		// 		<Route path={`${path}/registration`}>
-		// 			<Registration />
-		// 		</Route>
-		// 	</Switch>
 		// </>
+
+		<>
+			<h1>Getting Started</h1>
+			<span>
+				<Link to={`${url}/login`}>
+					<button>Login</button>
+				</Link>
+				<Link to={`${url}/registration`}>
+					<button>Register</button>
+				</Link>
+			</span>
+			<br />
+			<br />
+			<Switch>
+				<Route path={`${path}/login`}>
+					<PatientLogin />
+				</Route>
+				<Route path={`${path}/registration`}>
+					<PatientReg />
+				</Route>
+			</Switch>
+		</>
 	);
->>>>>>> ashley
 };
 
 export default PatientIntro;
