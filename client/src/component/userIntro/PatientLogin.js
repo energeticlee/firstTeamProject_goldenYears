b3700e468476665f6a5c9d0ca418a7794a307b39
“ash1ey” <xy.wang2208@gmail.com>
2021-07-26 12:39:35 +0800

    added dummy ReviewPerformance comp for doctors to review patient performance

--- client/src/component/userIntro/PatientLogin.js
+++ client/src/component/userIntro/PatientLogin.js
@@ -2,67 +2,67 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 const PatientLogin = () => {
-  const history = useHistory();
-  const [errorMessage, setMessage] = useState("Please enter your details");
-  const handleUserLogin = (event) => {
-    event.preventDefault();
-    const userEmail = event.target.email.value;
-    const userPassword = event.target.password.value;
+	const history = useHistory();
+	const [errorMessage, setMessage] = useState("Please enter your details");
+	const handleUserLogin = (event) => {
+		event.preventDefault();
+		const userEmail = event.target.email.value;
+		const userPassword = event.target.password.value;
 
-    const sendData = async () => {
-      // Please change the localhose number according to your server port number
-      const response = await fetch("http://localhost:3001/api/session/new", {
-        method: "POST",
-        mode: "cors",
-        body: JSON.stringify({
-          password: userPassword,
-          email: userEmail,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-      });
-      if (response.status === 400) {
-        const data = await response.json();
-        console.log("data", data.error);
-        setMessage(data.error);
-      } else if (response.status === 200) {
-        history.push("/home");
-      }
-    };
-    sendData();
-  };
-  return (
-    <div>
-      <form onSubmit={handleUserLogin}>
-        <label>Email:</label>
-        <br />
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email address"
-          required
-        />
-        <br />
-        <br />
-        <label>Password:</label>
-        <br />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Enter password"
-          required
-        />
-        <br />
-        <br />
-        <input type="submit" value="Enter" />
-      </form>
-      <div>{errorMessage}</div>
-    </div>
-  );
+		const sendData = async () => {
+			// Please change the localhose number according to your server port number
+			const response = await fetch("http://localhost:3001/api/session/new", {
+				method: "POST",
+				mode: "cors",
+				body: JSON.stringify({
+					password: userPassword,
+					email: userEmail,
+				}),
+				headers: {
+					"Content-Type": "application/json",
+					Accept: "application/json",
+				},
+			});
+			if (response.status === 400) {
+				const data = await response.json();
+				console.log("data", data.error);
+				setMessage(data.error);
+			} else if (response.status === 200) {
+				history.push("/home");
+			}
+		};
+		sendData();
+	};
+	return (
+		<div>
+			<form onSubmit={handleUserLogin}>
+				<label>Email:</label>
+				<br />
+				<input
+					type="email"
+					name="email"
+					id="email"
+					placeholder="Email address"
+					required
+				/>
+				<br />
+				<br />
+				<label>Password:</label>
+				<br />
+				<input
+					type="password"
+					name="password"
+					id="password"
+					placeholder="Enter password"
+					required
+				/>
+				<br />
+				<br />
+				<input type="submit" value="Enter" />
+			</form>
+			<div>{errorMessage}</div>
+		</div>
+	);
 };
 
 export default PatientLogin;
