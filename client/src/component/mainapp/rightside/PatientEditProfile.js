2fac8dd5d0691232ea241a6dbd1d31113b705883
“ash1ey” <xy.wang2208@gmail.com>
2021-07-26 17:53:25 +0800

    updated PatientEditProfile, need to update MyProfile - thanks Justin for your help

--- client/src/component/mainapp/rightside/PatientEditProfile.js
+++ client/src/component/mainapp/rightside/PatientEditProfile.js
@@ -6,6 +6,8 @@ const PatientEditProfile = () => {
 	const contextData = useContext(dataContext);
 	const states = contextData.states;
 	console.log(states);
+	console.log(dataContext);
+	console.log(contextData);
 	const [data, setUserData] = useState([]);
 	useEffect(() => {
 		const getData = async () => {
@@ -31,126 +33,122 @@ const PatientEditProfile = () => {
 	return (
 		<>
 			<div>
-				`
-				{data.map((userElement) => {
-					<form>
-						return ( key={userElement._id}
-						<label>Name:</label>
-						<br />
-						<input
-							type="text"
-							name="name"
-							id="name"
-							placeholder={userElement.name}
-							required
-						/>
-						<br />
-						<br />
-						<label>Email:</label>
-						<br />
-						<input
-							type="email"
-							name="email"
-							id="email"
-							placeholder={userElement.email}
-							required
-						/>
-						<br />
-						<br />
-						<label>Password:</label>
-						<br />
-						<input
-							type="password"
-							name="password"
-							id="password"
-							placeholder={userElement.password}
-							required
-						/>
-						<br />
-						<br />
-						<label>Photo:</label>
-						<br />
-						<input
-							type="file"
-							name="photo"
-							id="photo"
-							placeholder={userElement.photo}
-							accept="image/png. image/jpeg"
-							required
-						/>
-						<br />
-						<br />
-						<label>Age:</label>
-						<br />
-						<input
-							type="number"
-							name="age"
-							id="age"
-							placeholder={userElement.age}
-							required
-						/>
-						<br />
-						<br />
-						<label>Gender:</label>
-						<br />
-						<input
-							type="text"
-							name="gender"
-							id="gender"
-							placeholder={userElement.gender}
-							required
-						/>
-						<br />
-						<br />
-						<label>Height:</label>
-						<br />
-						<input
-							type="number"
-							name="height"
-							id="height"
-							placeholder={userElement.height}
-							required
-						/>
-						<br />
-						<br />
-						<label>Weight:</label>
-						<br />
-						<input
-							type="number"
-							name="weight"
-							id="weight"
-							placeholder={userElement.weight}
-							required
-						/>
-						<br />
-						<br />
-						<label>Health Conditions:</label>
-						<br />
-						<input
-							type="text"
-							name="healthConditions"
-							id="healthConditions"
-							placeholder={userElement.healthCondition}
-							required
-						/>
-						<br />
-						<br />
-						<label>My Doctor:</label>
-						<br />
-						<input
-							type="text"
-							name="myDoctor"
-							id="myDoctor"
-							placeholder={userElement.myDoctor}
-							required
-						/>
-						<br />
-						<br />
-						<input type="submit" value="Save Changes" />
-						);
-					</form>;
-				})}
-				`
+				<form>
+					<label>Name:</label>
+					<br />
+					<input
+						type="text"
+						name="name"
+						id="name"
+						placeholder={data.name}
+						required
+					/>
+					<br />
+					<br />
+					<label>Email:</label>
+					<br />
+					<input
+						type="email"
+						name="email"
+						id="email"
+						placeholder={data.email}
+						required
+					/>
+					<br />
+					<br />
+					<label>Password:</label>
+					<br />
+					<input
+						type="password"
+						name="password"
+						id="password"
+						placeholder={data.password}
+						required
+					/>
+					<br />
+					<br />
+					<label>Photo:</label>
+					<br />
+					<input
+						type="file"
+						name="photo"
+						id="photo"
+						placeholder={data.photo}
+						accept="image/png. image/jpeg"
+						required
+					/>
+					<br />
+					<br />
+					<label>Age:</label>
+					<br />
+					<input
+						type="number"
+						name="age"
+						id="age"
+						placeholder={data.age}
+						required
+					/>
+					<br />
+					<br />
+					<label>Gender:</label>
+					<br />
+					<input
+						type="text"
+						name="gender"
+						id="gender"
+						placeholder={data.gender}
+						required
+					/>
+					<br />
+					<br />
+					<label>Height:</label>
+					<br />
+					<input
+						type="number"
+						name="height"
+						id="height"
+						placeholder={data.height}
+						required
+					/>
+					<br />
+					<br />
+					<label>Weight:</label>
+					<br />
+					<input
+						type="number"
+						name="weight"
+						id="weight"
+						placeholder={data.weight}
+						required
+					/>
+					<br />
+					<br />
+					<label>Health Conditions:</label>
+					<br />
+					<input
+						type="text"
+						name="healthConditions"
+						id="healthConditions"
+						placeholder={data.healthCondition}
+						required
+					/>
+					<br />
+					<br />
+					<label>My Doctor:</label>
+					<br />
+					<input
+						type="text"
+						name="myDoctor"
+						id="myDoctor"
+						placeholder={data.myDoctor}
+						required
+					/>
+					<br />
+					<br />
+					<input type="submit" value="Save Changes" />
+					);
+				</form>
+				)
 			</div>
 		</>
 	);
