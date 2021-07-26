f55b6acc029de67dacfa9ae51dd8144f6f92effe
Kevin Ho <hokevin78@gmail.com>
2021-07-26 12:36:58 +0800

    refactored routes in App.js

--- client/src/component/mainapp/Landing.js
+++ client/src/component/mainapp/Landing.js
@@ -1,28 +1,16 @@
 import React from "react";
 import HomeNavBar from "./HomeNavBar";
-import DoctorIntro from "../userIntro/DocIntro";
-import PatientIntro from "../userIntro/PatientIntro";
-import { Switch, Route, useRouteMatch } from "react-router-dom";
 
 const Landing = () => {
-	const { path } = useRouteMatch();
-	return (
-		<div>
-			<HomeNavBar />
-			<h1>GoldenYears</h1>
-			<p>
-				Curabitur vestibulum aliquam leo. Etiam feugiat lorem non metus.
-				Praesent blandit laoreet nibh. Ut leo.
-			</p>
-			<Switch>
-				<Route path={`${path}/patients`}>
-					<PatientIntro />
-				</Route>
-				<Route path={`${path}/doctors`}>
-					<DoctorIntro />
-				</Route>
-			</Switch>
-		</div>
-	);
+  return (
+    <div>
+      <HomeNavBar />
+      <h1>GoldenYears</h1>
+      <p>
+        Curabitur vestibulum aliquam leo. Etiam feugiat lorem non metus.
+        Praesent blandit laoreet nibh. Ut leo.
+      </p>
+    </div>
+  );
 };
 export default Landing;
