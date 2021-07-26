3922c4cb27d75907c04f7fbe8741ca08600ed336
“ash1ey” <xy.wang2208@gmail.com>
2021-07-26 11:30:29 +0800

    updated landing page route on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,19 @@ import { Switch, Route } from "react-router-dom";
 import Landing from "./component/mainapp/Landing";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>GoldenYears</h1>
-      <Switch>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="/landing">
-          <Landing />
-        </Route>
-      </Switch>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>GoldenYears</h1>
+			<Switch>
+				<Route exact path="/">
+					<Landing />
+				</Route>
+				<Route path="/home">
+					<Home />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
