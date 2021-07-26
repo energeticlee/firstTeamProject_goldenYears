316a7263c2b5f1fd85fb0183bd994d846d00d20c
Kevin Ho <hokevin78@gmail.com>
2021-07-26 09:13:41 +0800

    Create fetch post method in user Registration

--- controller/user.js
+++ controller/user.js
@@ -2,13 +2,36 @@ const express = require("express");
 const router = express.Router();
 const userSchema = require("../models/user");
 
-router.get("/", (req, res) => {
-  userSchema.find({}, (err, foundUser) => {
+// Get One user by Id
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  userSchema.findById(id, (err, foundUser) => {
     if (err) {
       res.status(400).json({ error: err.message });
     } else {
       res.status(200).json(foundUser);
     }
+  });
+});
+
+// Get all users
+router.get("/", (req, res) => {
+  userSchema.find({}, (err, foundUsers) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else {
+      res.status(200).json(foundUsers);
+    }
+  });
+});
+
+// Create User
+router.post("/", (req, res) => {
+  userSchema.create(req.body, (error, createdUser) => {
+    if (error) {
+      res.status(400).json({ error: error.message });
+    }
+    res.status(200).json(createdUser);
   });
 });
 
