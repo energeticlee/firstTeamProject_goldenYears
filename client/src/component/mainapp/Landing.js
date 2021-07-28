1620a5c95758a65fe00225584466ab491be7b5c2
“ash1ey” <xy.wang2208@gmail.com>
2021-07-28 15:36:57 +0800

    added some custom CSS to landing page

--- client/src/component/mainapp/Landing.js
+++ client/src/component/mainapp/Landing.js
@@ -2,13 +2,18 @@ import React from "react";
 import HomeNavBar from "./HomeNavBar";
 
 const Landing = () => {
-  return (
-    <div>
-      <HomeNavBar />
-      <h1 className="is-size-1 has-text-centered has-text-primary">
-        GoldenYears
-      </h1>
-    </div>
-  );
+	return (
+		<div>
+			<HomeNavBar />
+			<h1 className="is-size-1 has-text-centered has-text-primary">
+				GoldenYears
+			</h1>
+			<p className="content is-medium has-text-centered">
+				Make the sunset years of your life... Donec mi odio, faucibus at,
+				scelerisque quis, convallis in, nisi. Pellentesque auctor neque nec
+				urna. Morbi nec metus.
+			</p>
+		</div>
+	);
 };
 export default Landing;
