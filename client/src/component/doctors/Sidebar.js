fa7febde5e56a15c66b663411bfa463dfa1ee8ed
“ash1ey” <xy.wang2208@gmail.com>
2021-07-28 23:52:53 +0800

    added some CSS

--- client/src/component/doctors/Sidebar.js
+++ client/src/component/doctors/Sidebar.js
@@ -2,41 +2,46 @@ import React from "react";
 import { Link, useRouteMatch } from "react-router-dom";
 
 const Sidebar = () => {
-  let { url } = useRouteMatch();
+	let { url } = useRouteMatch();
 
-  const logout = () => {
-    // Please change the localhose number according to your server port number
-    fetch("/api/doctorSession", {
-      method: "DELETE",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    });
-  };
+	const logout = () => {
+		// Please change the localhose number according to your server port number
+		fetch("/api/doctorSession", {
+			method: "DELETE",
+			mode: "cors",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+			},
+		});
+	};
 
-  return (
-    <div>
-      <ul>
-        <li>
-          {" "}
-          <Link to={`${url}/myprofile`}> My Profile </Link>{" "}
-        </li>
-        <li>
-          <Link to={`${url}/mypatients`}>My Patients</Link>
-        </li>
-        <li>
-          <Link to={`${url}/overallperformance`}>Overall Performance</Link>
-        </li>
-        <li>
-          <Link to={"/"} onClick={logout}>
-            Log out
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<>
+			<nav>
+				<aside className="menu">
+					<p className="is-size-3 sidebar-title">GoldenYears</p>
+					<ul className="menu-list is-size-5 sidebar-items">
+						<li>
+							{" "}
+							<Link to={`${url}/myprofile`}> My Profile </Link>{" "}
+						</li>
+						<li>
+							<Link to={`${url}/mypatients`}>My Patients</Link>
+						</li>
+						<li>
+							<Link to={`${url}/overallperformance`}>Overall Performance</Link>
+						</li>
+						<li>
+							<Link to={"/"} onClick={logout}>
+								Log out
+							</Link>
+						</li>
+					</ul>
+				</aside>
+			</nav>
+		</>
+	);
 };
 
 export default Sidebar;
