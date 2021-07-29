b255117b147676d80d13abd70b8bc34a12dd90a7
“ash1ey” <xy.wang2208@gmail.com>
2021-07-29 22:41:52 +0800

    iwan2die

--- client/src/component/mainapp/Landing.js
+++ client/src/component/mainapp/Landing.js
@@ -23,14 +23,14 @@ const Landing = () => {
 			</p>
 
 			<div
-				id="carouselExampleCaptions"
+				id="carouselExampleIndicators"
 				className="carousel slide"
 				data-bs-ride="carousel"
 			>
 				<div className="carousel-indicators">
 					<button
 						type="button"
-						data-bs-target="#carouselExampleCaptions"
+						data-bs-target="#carouselExampleIndicators"
 						data-bs-slide-to="0"
 						className="active"
 						aria-current="true"
@@ -38,50 +38,41 @@ const Landing = () => {
 					></button>
 					<button
 						type="button"
-						data-bs-target="#carouselExampleCaptions"
+						data-bs-target="#carouselExampleIndicators"
 						data-bs-slide-to="1"
 						aria-label="Slide 2"
 					></button>
 					<button
 						type="button"
-						data-bs-target="#carouselExampleCaptions"
+						data-bs-target="#carouselExampleIndicators"
 						data-bs-slide-to="2"
 						aria-label="Slide 3"
 					></button>
 				</div>
 				<div className="carousel-inner">
 					<div className="carousel-item active">
-						<img src="..." className="d-block w-100" alt="..." />
-						<div className="carousel-caption d-none d-md-block">
-							<h5>First slide label</h5>
-							<p>
-								Some representative placeholder content for the first slide.
-							</p>
-						</div>
+						<img
+							src="https://i.imgur.com/EkZ5jjH.png"
+							className="d-block w-100"
+						/>
 					</div>
 					<div className="carousel-item">
-						<img src="..." className="d-block w-100" alt="..." />
-						<div className="carousel-caption d-none d-md-block">
-							<h5>Second slide label</h5>
-							<p>
-								Some representative placeholder content for the second slide.
-							</p>
-						</div>
+						<img
+							src="https://i.imgur.com/FXKKM7x.png"
+							className="d-block w-100"
+						/>
 					</div>
 					<div className="carousel-item">
-						<img src="..." className="d-block w-100" alt="..." />
-						<div className="carousel-caption d-none d-md-block">
-							<h5>Third slide label</h5>
-							<p>
-								Some representative placeholder content for the third slide.
-							</p>
-						</div>
+						<img
+							src="https://i.imgur.com/IHPqTKt.png"
+							className="d-block w-100"
+						/>
 					</div>
 				</div>
 				<button
 					className="carousel-control-prev"
 					type="button"
-					data-bs-target="#carouselExampleCaptions"
+					data-bs-target="#carouselExampleIndicators"
 					data-bs-slide="prev"
 				>
 					<span
@@ -93,7 +84,7 @@ const Landing = () => {
 				<button
 					className="carousel-control-next"
 					type="button"
-					data-bs-target="#carouselExampleCaptions"
+					data-bs-target="#carouselExampleIndicators"
 					data-bs-slide="next"
 				>
 					<span
