69d4ceb276c015f8253cd2dfdee1f98acd1d0947
farhan <mzulfarhan94@gmail.com>
2021-07-30 10:52:57 +0800

    hey

--- models/faketestdata2.js
+++ models/faketestdata2.js
@@ -4,44 +4,146 @@
 //* const test = parseInt(Date.now)
 
 module.exports.faketestdata2 = [
-    {
-      date: 1610640000000,
-      result: 20,
-      user: "6100cf299d6bc9936ef1dbac"
-    },
-    {
-      date: 1613750400000,
-      result: 10,
-      user: "6100cf299d6bc9936ef1dbac"
-    },
-    {
-      date: 1617033600000,
-      result: 20,
-      user: "6100cf299d6bc9936ef1dbac"
-    },
-    {
-      date: 1617206400000,
-      result: 10,
-      user: "6100cf299d6bc9936ef1dbac"
-    },
-    {
-      date: 1621440000000,
-      result: 15,
-      user: "6100cf299d6bc9936ef1dbac"
-    },
-    {
-      date: 1624982400000,
-      result: 5,
-      user: "6100cf299d6bc9936ef1dbac"
-    },
-    {
-      date: 1626278400000,
-      result: 11,
-      user: "6100cf299d6bc9936ef1dbac"
-    },
-    {
-      date: 1628524800000,
-      result: 14,
-      user: "60fe76b958ba8d363f597e84"
-    },
+  {
+    date: 1610640000000,
+    result: 5,
+    user: "6100cf299d6bc9936ef1dbac"
+  },
+  {
+    date: 1613750400000,
+    result: 20,
+    user: "6100cf299d6bc9936ef1dbac"
+  },
+  {
+    date: 1617033600000,
+    result: 10,
+    user: "6100cf299d6bc9936ef1dbac"
+  },
+  {
+    date: 1617206400000,
+    result: 5,
+    user: "6100cf299d6bc9936ef1dbac"
+  },
+  {
+    date: 1621440000000,
+    result: 16,
+    user: "6100cf299d6bc9936ef1dbac"
+  },
+  {
+    date: 1624982400000,
+    result: 25,
+    user: "6100cf299d6bc9936ef1dbac"
+  },
+  {
+    date: 1626278400000,
+    result: 3,
+    user: "6100cf299d6bc9936ef1dbac"
+  },
+
+  {
+    date: 1610640000000,
+    result: 5,
+    user: "6103549f4ae6b90022b05747"
+  },
+  {
+    date: 1613750400000,
+    result: 20,
+    user: "6103549f4ae6b90022b05747"
+  },
+  {
+    date: 1617033600000,
+    result: 10,
+    user: "6103549f4ae6b90022b05747"
+  },
+  {
+    date: 1617206400000,
+    result: 5,
+    user: "6103549f4ae6b90022b05747"
+  },
+  {
+    date: 1621440000000,
+    result: 16,
+    user: "6103549f4ae6b90022b05747"
+  },
+  {
+    date: 1624982400000,
+    result: 25,
+    user: "6103549f4ae6b90022b05747"
+  },
+  {
+    date: 1626278400000,
+    result: 3,
+    user: "6103549f4ae6b90022b05747"
+  },
+  {
+    date: 1610640000000,
+    result: 5,
+    user: "6103550c4ae6b90022b0575b"
+  },
+  {
+    date: 1613750400000,
+    result: 20,
+    user: "6103550c4ae6b90022b0575b"
+  },
+  {
+    date: 1617033600000,
+    result: 10,
+    user: "6103550c4ae6b90022b0575b"
+  },
+  {
+    date: 1617206400000,
+    result: 5,
+    user: "6103550c4ae6b90022b0575b"
+  },
+  {
+    date: 1621440000000,
+    result: 16,
+    user: "6103550c4ae6b90022b0575b"
+  },
+  {
+    date: 1624982400000,
+    result: 25,
+    user: "6103550c4ae6b90022b0575b"
+  },
+  {
+    date: 1626278400000,
+    result: 3,
+    user: "6103550c4ae6b90022b0575b"
+  },
+
+  {
+    date: 1610640000000,
+    result: 5,
+    user: "6103565b4ae6b90022b057a4"
+  },
+  {
+    date: 1613750400000,
+    result: 20,
+    user: "6103565b4ae6b90022b057a4"
+  },
+  {
+    date: 1617033600000,
+    result: 10,
+    user: "6103565b4ae6b90022b057a4"
+  },
+  {
+    date: 1617206400000,
+    result: 5,
+    user: "6103565b4ae6b90022b057a4"
+  },
+  {
+    date: 1621440000000,
+    result: 16,
+    user: "6103565b4ae6b90022b057a4"
+  },
+  {
+    date: 1624982400000,
+    result: 25,
+    user: "6103565b4ae6b90022b057a4"
+  },
+  {
+    date: 1626278400000,
+    result: 3,
+    user: "6103565b4ae6b90022b057a4"
+  },
   ];
--- models/faketestdata3.js
+++ models/faketestdata3.js
@@ -11,17 +11,17 @@ module.exports.faketestdata3 = [
   },
   {
     date: 1613750400000,
-    result: 15,
+    result: 7,
     user: "6100cf299d6bc9936ef1dbac"
   },
   {
     date: 1617033600000,
-    result: 30,
+    result: 10,
     user: "6100cf299d6bc9936ef1dbac"
   },
   {
     date: 1617206400000,
-    result: 1,
+    result: 12,
     user: "6100cf299d6bc9936ef1dbac"
   },
   {
@@ -31,17 +31,119 @@ module.exports.faketestdata3 = [
   },
   {
     date: 1624982400000,
-    result: 8,
+    result: 20,
     user: "6100cf299d6bc9936ef1dbac"
   },
   {
     date: 1626278400000,
-    result: 25,
+    result: 1,
     user: "6100cf299d6bc9936ef1dbac"
   },
+
   {
-    date: 1628524800000,
-    result: 25,
-    user: "6100cf299d6bc9936ef1dbac"
+    date: 1610640000000,
+    result: 6,
+    user: "6103549f4ae6b90022b05747"
+  },
+  {
+    date: 1613750400000,
+    result: 7,
+    user: "6103549f4ae6b90022b05747"
+  },
+  {
+    date: 1617033600000,
+    result: 10,
+    user: "6103549f4ae6b90022b05747"
+  },
+  {
+    date: 1617206400000,
+    result: 3,
+    user: "6103549f4ae6b90022b05747"
+  },
+  {
+    date: 1621440000000,
+    result: 31,
+    user: "6103549f4ae6b90022b05747"
+  },
+  {
+    date: 1624982400000,
+    result: 21,
+    user: "6103549f4ae6b90022b05747"
+  },
+  {
+    date: 1626278400000,
+    result: 4,
+    user: "6103549f4ae6b90022b05747"
+  },
+  {
+    date: 1610640000000,
+    result: 8,
+    user: "6103550c4ae6b90022b0575b"
+  },
+  {
+    date: 1613750400000,
+    result: 15,
+    user: "6103550c4ae6b90022b0575b"
+  },
+  {
+    date: 1617033600000,
+    result: 19,
+    user: "6103550c4ae6b90022b0575b"
+  },
+  {
+    date: 1617206400000,
+    result: 21,
+    user: "6103550c4ae6b90022b0575b"
+  },
+  {
+    date: 1621440000000,
+    result: 11,
+    user: "6103550c4ae6b90022b0575b"
+  },
+  {
+    date: 1624982400000,
+    result: 13,
+    user: "6103550c4ae6b90022b0575b"
+  },
+  {
+    date: 1626278400000,
+    result: 14,
+    user: "6103550c4ae6b90022b0575b"
+  },
+
+  {
+    date: 1610640000000,
+    result: 3,
+    user: "6103565b4ae6b90022b057a4"
+  },
+  {
+    date: 1613750400000,
+    result: 31,
+    user: "6103565b4ae6b90022b057a4"
+  },
+  {
+    date: 1617033600000,
+    result: 21,
+    user: "6103565b4ae6b90022b057a4"
+  },
+  {
+    date: 1617206400000,
+    result: 3,
+    user: "6103565b4ae6b90022b057a4"
+  },
+  {
+    date: 1621440000000,
+    result: 4,
+    user: "6103565b4ae6b90022b057a4"
+  },
+  {
+    date: 1624982400000,
+    result: 20,
+    user: "6103565b4ae6b90022b057a4"
+  },
+  {
+    date: 1626278400000,
+    result: 3,
+    user: "6103565b4ae6b90022b057a4"
   },
   ];
